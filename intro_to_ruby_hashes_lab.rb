367a4bbf2c3a42e9804598cff6057601d4a39e2f
Timur Mishiev <timurmishiev@gmail.com>
2020-02-02 02:34:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,12 +8,14 @@ actor = {:name => "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {:railroad => {}
+	monopoly = { 
+	  :railroads=>{}
 	 	}
 end
 
 def monopoly_with_second_tier
-monopoly = {:railroad => { 
+monopoly = {
+  :railroad => { 
   
 }
 	 	}
